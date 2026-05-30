55b3fbce8e34830632049b2d87d8a0fb0e224576
Jhoset Fabian <lopezrenteriajhosetfabian@gmail.com>
2026-05-29 19:23:58 -0500

    .

---
+++ cap1_Introduccion/ex04/Ejercicio4.js
@@ -0,0 +1,23 @@
+let nombre, edad, direccion, movil, email; //Se pueden declarar varias variables del mismo tipo.
+
+//prompt: es una palabra reservada en javascript que despliega una funcion y sale un mensaje de tipo alert para el usuario.
+
+nombre = prompt("Escriba su nombre: ")//Pedir datos al usuario.
+document.write("Bienvenido: ", nombre, "<br>");
+console.log("Bienvenido: ", nombre);
+
+edad = prompt("Escriba su edad: ")//Pedir datos al usuario.
+document.write("Tu edad es: ", edad, "<br>");
+console.log("Tu edad es: ", edad);
+
+direccion = prompt("Escriba su direccion: ")//Pedir datos al usuario.
+document.write("Tu direccion es: ", direccion, "<br>");
+console.log("Tu direccion es: ", direccion);
+
+movil = prompt("Escriba su movil: ")//Pedir datos al usuario.
+document.write("Tu movil es: ", movil, "<br>");
+console.log("Tu movil es: ", movil);
+
+email = prompt("Escriba su email: ")//Pedir datos al usuario.
+document.write("Tu email es: ", email, "<br>");
+console.log("Tu email es: ", email);
