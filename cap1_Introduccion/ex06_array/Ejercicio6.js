2dd215e81527a51e824228fbe783a5cbce6e26f3
Jhoset Fabian <lopezrenteriajhosetfabian@gmail.com>
2026-06-04 21:20:21 -0500

    Condiciones

---
+++ cap1_Introduccion/ex06_array/Ejercicio6.js
@@ -0,0 +1,13 @@
+/* ARRAYS O ARREGLOS
+Un array es una estructura de datos que se utiliza para almacenar una colección ordenada de elementos.
+Estos elementos pueden ser cadenas de texto, valores numericos como: enteros, decimales; valores booleanos, entre otros. Pueden ser tambien un conjunto de funciones e inclusive otros arrays.
+*/
+
+let Frutas = ["Piña", "Maracuya", "Banano", "Fresa", "Mango"] // Array de cadenas de texto representando frutas.
+
+console.log(Frutas);
+console.log(Frutas[0]);
+console.log(Frutas[1]);
+console.log(Frutas[2]);
+console.log(Frutas[3]);
+console.log(Frutas[4]);
