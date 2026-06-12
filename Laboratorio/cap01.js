8e8c655d3e08903662868bea530cb1ff30c7e9cc
Kevin <kevinse24@gmail.com>
2026-06-11 21:15:59 -0500

    Laboratorio

---
+++ Laboratorio/cap01.js
@@ -0,0 +1,48 @@
+/*
+=================================
+INTRODUCCION A JAVASCRIPT
+=================================
+
+JavaScript es un lenguaje de programacion
+que permite agregar comportamiento a las paginas web.
+
+Todo el codigo de este archivo será ejecutado por el
+por el navegador de arriba hacia abajo.
+*/
+
+console.log("Hola Mundo!");
+
+/*
+================================
+MULTIPLES MENSAJES
+================================
+
+console.log() permite mostrar informacion
+en la consola del navegador.
+*/
+
+console.log("Mensaje 1");
+console.log("Mensaje 2");
+console.log("Mensaje 3");
+console.log("Mensaje 4");
+
+/*
+================================
+COMENTARIOS
+================================
+
+Los comentarios sirven para documentar
+el codigo.
+
+El navegador los ignora.
+*/
+
+// comentario de una linea
+
+/*
+comentario
+de múltiples
+líneas
+*/
+
+console.log("Los comentarios no aparecen");
